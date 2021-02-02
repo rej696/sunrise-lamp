562ac8c0e9c4e9f5f8f757fb7de964aad1dbdaa6
sally <55083730+rej696@users.noreply.github.com>
2021-02-02 00:10:44 +0000

    start sunrise light function on accessing website

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
+from time import asctime
 from flask import Flask
+import subprocess
+
 app = Flask(__name__)
+
+def sunrise():
+    header = f"\nSunrise @ {asctime()}:\n"
+
+    with open("/home/pi/sunrise-lamp/output.txt", "a+") as log:
+        log.write(header)
+        
+        with open("/home/pi/sunrise-lamp/error.txt", "a+") as err:
+            err.write(header)
+            
+            subprocess.Popen(["python3", "sunrise.py"], stdout=log, stderr=err)
+
+
 
 @app.route("/")
 def hello():
+    sunrise()
+    
     return "Hi Sally, hope you are enjoying your lamp"
 
 if __name__ == "__main__":
